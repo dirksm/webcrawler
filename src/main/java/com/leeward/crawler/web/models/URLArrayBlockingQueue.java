55e09ecf62a074dbb69a12aa3f02d5f270d1458d
Michael Dirks <mdirks@leewardassociates.com>
2016-09-06 16:06:37 -0500

    added methods

--- src/main/java/com/leeward/crawler/web/models/URLArrayBlockingQueue.java
+++ src/main/java/com/leeward/crawler/web/models/URLArrayBlockingQueue.java
@@ -1,7 +1,9 @@
 package com.leeward.crawler.web.models;
 
+import java.util.List;
 import java.util.TreeSet;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This is a unique ArrayBlockingQueue ensuring that no duplicate URL's are added to the queue.
@@ -19,11 +21,34 @@ public class URLArrayBlockingQueue<E> extends ArrayBlockingQueue<String> {
 		urls = new TreeSet<String>();
 	}
 	
+	public void addAll(List<String> e) {
+		for (String s : e) {
+			this.offer(s);
+		}
+	}
+	
 	@Override
 	public void put(String e) throws InterruptedException {
 		if (urls.add(e)) {
 			super.put(e);
 		}
 	}
+	
+	@Override
+	public boolean offer(String e) {
+		boolean o = false;
+		if (urls.add(e)) {
+			o = super.offer(e);
+		}
+		return o;
+	}
 
+	@Override
+	public boolean offer(String e, long timeout, TimeUnit unit) throws InterruptedException {
+		boolean o = false;
+		if (urls.add(e)) {
+			o = super.offer(e, timeout, unit);
+		}
+		return o;
+	}
 }
