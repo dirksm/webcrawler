2da130dfee094a797627b08992b73fdfffecf825
Michael Dirks <mdirks@leewardassociates.com>
2016-09-07 17:03:16 -0500

    new changes for search

---
+++ src/main/java/com/leeward/crawler/web/controllers/SearchController.java
@@ -0,0 +1,27 @@
+package com.leeward.crawler.web.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.leeward.crawler.web.models.SearchResultModel;
+import com.leeward.crawler.web.services.SearchService;
+
+@RestController
+@RequestMapping(path="/search")
+public class SearchController {
+
+	@Autowired
+	SearchService searchService;
+	
+	@RequestMapping(value="/{url}/{q}", method=RequestMethod.GET)
+	public List<SearchResultModel> search(@PathVariable String url, @PathVariable String q) {
+		System.out.println("url: " + url);
+		System.out.println("q: " + q);
+		return searchService.search("http://"+url, q);
+	}
+}
