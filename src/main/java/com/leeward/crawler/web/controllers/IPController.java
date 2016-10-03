bea92ad0f25e023682e7ff9b75b7a92411df55fb
Michael Dirks <mdirks@leewardassociates.com>
2016-10-03 10:01:56 -0500

    new ip controller

--- src/main/java/com/leeward/crawler/web/controllers/IPController.java
+++ src/main/java/com/leeward/crawler/web/controllers/IPController.java
@@ -1,5 +1,10 @@
 package com.leeward.crawler.web.controllers;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,4 +29,22 @@ public class IPController {
 		map.addAttribute("ip", ip);
 		return "ip";
 	}
+
+	@RequestMapping(value="/location/")
+	public String getIPLocation(ModelMap map, HttpServletRequest request) {
+		RestTemplate template = new RestTemplate();
+		String ip = request.getHeader("X-FORWARDED-FOR");
+		if (StringUtils.isNotBlank(ip)) {
+			ip = ip.replaceFirst(",.*", "");
+		} else {
+			ip = request.getRemoteAddr();
+		}
+		String url = "http://localhost:8080/geoip/search/"+ip;
+		JsonStatusModel json = template.getForObject(url, JsonStatusModel.class);
+		map.addAttribute("status", json.getStatus());
+		map.addAttribute("geoip", json.getResult());
+		map.addAttribute("ip", ip);
+		return "ip";
+	}
+
 }
